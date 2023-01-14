34e231d42b08f2831bf7ad35ca96f395e81d0021
9beach <9beach@gmail.com>
2023-01-14 20:19:57 +0900

    Add some other reels

---
+++ Aftersun (2022)/srt-conv.sh
@@ -0,0 +1,1 @@
+srttidy -l '00:01:09,832->00:01:34,600 01:32:18,020->01:32:47,807' < Aftersun.2022.1080p.WEBRip.AAC2.0.x264-NOGRP.ko.srt > aftersun.2022.1080p.web.h264-naisu.ko.srt
---
+++ Gretel and Hansel (2020)/srt-conv.sh
@@ -0,0 +1,2 @@
+srttidy -s 0.6 < Gretel.and.Hansel.2020.1080p.WEB-DL.DD5.1.x264-CMRG.ko.srt > 'Gretel and Hansel.2020.1080p.Bluray.DTS-HD.MA.5.1.X264-EVO.ko.srt'
+cp -f Gretel.and.Hansel.2020.1080p.WEB-DL.DD5.1.x264-CMRG.ko.srt 'Gretel & Hansel (2020) (2160p BluRay x265 10bit HDR Tigole).ko.srt'
